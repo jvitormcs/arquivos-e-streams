a62bff8a39e8e6f3dffe173c4f1ae5a2eb72a97d
jvitormcs <vitormouracs@gmail.com>
2022-01-27 09:10:19 -0300

    file and fileInfo concluido

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Digite o nome do arquivo");
+var nome = ReadLine();
+nome = LimparNome(nome);
+
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.text");
+
+CriarArquivo(path);
+
+WriteLine("Digite enter para finilizar...");
+ReadLine();
+
+static string LimparNome(string nome){
+    foreach (var @char in Path.GetInvalidFileNameChars())
+    {   
+    nome = nome.Replace(@char, '-');
+    }
+    return nome;
+}
+
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Essa é a linha um do arquivo");
+        sw.WriteLine("Essa é a linha dois do arquivo");
+        sw.WriteLine("Essa é a linha três do arquivo");
+
+    }
+    catch
+    {
+        WriteLine("O nome do arquivo está invalido");
+    }
+}
