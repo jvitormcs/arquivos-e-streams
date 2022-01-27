ae28c15e21a540f28011af0a0e3e65ba7bb97bb5
jvitormcs <vitormouracs@gmail.com>
2022-01-27 10:40:49 -0300

    Criação do diretorio globo e seus subdiretorios para continente e paises

---
+++ Directory_And_DirectoryInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "globo");
+var dirGlobo = Directory.CreateDirectory(path);
+var dirAmNorte =  dirGlobo.CreateSubdirectory("América Do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("América Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("América Do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canadá");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panama");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
