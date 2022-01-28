ce7419db25fdb4079b7cf032e5c77a6f643fc42a
jvitormcs <vitormouracs@gmail.com>
2022-01-27 23:34:42 -0300

    trabalhando com StringReader

---
+++ StringReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+
+sb.AppendLine("Caracteres na primeira linha para ler");
+sb.AppendLine("e a segunda linha");
+sb.AppendLine("e fim");
+
+using var sr = new StringReader(sb.ToString());
+/* var buffer = new char[10];
+var tamanho = 0; */
+
+do
+{
+
+    Console.WriteLine(sr.ReadLine());
+
+}while (sr.Peek() >= 0);
+
+
+/* do
+{
+    buffer = new char[10];
+    tamanho = sr.Read(buffer);
+    Console.Write($"{string.Join("", buffer)}");
+
+} while (tamanho >= buffer.Length); */
+
+Console.WriteLine("pressione [enter] para finalizar");
+Console.ReadKey();
